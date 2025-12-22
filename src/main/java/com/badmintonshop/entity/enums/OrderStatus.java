d5ea1d3416f02996a22170877395ec5ebf2953c3
Tuong2608 <tranvantuonggl017@gmail.com>
2025-12-22 20:38:22 +0700

    Refactor: Revert OrderStatus to match database (remove SHIPPING, RETURNED)

--- src/main/java/com/badmintonshop/entity/enums/OrderStatus.java
+++ src/main/java/com/badmintonshop/entity/enums/OrderStatus.java
@@ -6,10 +6,8 @@ public enum OrderStatus {
     PROCESSING, // Đang xử lý
     STRINGING, // Đang đan vợt
     READY_TO_SHIP, // Sẵn sàng giao
-    SHIPPING, // Đang giao hàng
     SHIPPED, // Đã giao cho vận chuyển
     DELIVERED, // Đã giao thành công
     CANCELLED, // Đã hủy
-    RETURNED, // Trả hàng/Hoàn tiền
     REFUNDED // Đã hoàn tiền
 }
